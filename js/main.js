cb3b368aecdad12e582595a31886631e938cca19
Adrian Claudiu Covaci <adrianclaudiucovaci@gmail.com>
2014-05-08 21:43:20 +0300

    Further improvements

--- js/main.js
+++ js/main.js
@@ -1,3 +1,30 @@
 var jumbotronHeightPercent = 0.75
+var menuToggled = 0;
 
-$(".home-j").css("height",($(window).height() - $("#navbar").height()) * jumbotronHeightPercent);
+function size() {
+	$(".home-j").css("height",($(window).height() - $("#navbar").height()) * jumbotronHeightPercent);
+}
+
+size();
+
+function sidebarToggle() {
+	if(menuToggled == 0) {
+		$("#mainpage").animate({marginLeft: "250px"})
+		$("footer").animate({left: "250px"})
+		menuToggled = 1;
+	} else {
+		$("#mainpage").animate({marginLeft: "0px"})
+		$("footer").animate({left: "0px"})
+		menuToggled = 0;
+	}
+}
+
+$(window).resize(function() {
+	size();
+})
+
+$(document).keyup(function (e) {
+	if (e.keyCode == 39) {
+		sidebarToggle()
+	}
+});
